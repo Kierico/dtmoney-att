196dd3ecbbc4f20a416521f1080c94fed4e9d773
Kiérico Souza <kiericograndao@gmail.com>
2022-09-05 10:13:12 -0400

    1.5 - Tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,38 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionContainer, TransactionTable } from "./styles";
 
 export function Transactions() {
     return (
         <div>
             <Header />
             <Summary />
+            <TransactionContainer>
+                <TransactionTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighLight variant="income">
+                                    R$ 12.000,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>05/09/2022</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Temaki</td>
+                            <td>
+                                <PriceHighLight variant="outcome">
+                                    - R$ 27,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>05/09/2022</td>
+                        </tr>
+                    </tbody>
+                </TransactionTable>
+            </TransactionContainer>
         </div>
     );
 }
